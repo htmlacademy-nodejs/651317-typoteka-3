ef46f67c1ee2f17d43474354e75df3ec75382d20
Edem <50662754+newfaceee@users.noreply.github.com>
2020-07-31 14:55:18 +0300

    Устанавливает шаблонизатор pug, фиксирует директорию с шаблонами

--- src/express/index.js
+++ src/express/index.js
@@ -1,14 +1,19 @@
-'use strict';
+"use strict";
 
 const express = require(`express`);
+const path = requrie(`path`);
 
 const articleRoutes = require(`./routes/article-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 
-const {EXPRESS_DEFAULT_PORT} = require(`../constants`);
+const { EXPRESS_DEFAULT_PORT } = require(`../constants`);
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/articles`, articleRoutes);
 app.use(`/`, mainRoutes);
