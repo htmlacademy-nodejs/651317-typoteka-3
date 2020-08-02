ab0b0778ecd5f16e8e30e77a76e5d4402e15ec87
Edem <50662754+newfaceee@users.noreply.github.com>
2020-08-02 08:24:25 +0300

    Обрабатывает 500 и 404 ошибку

--- src/express/index.js
+++ src/express/index.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const express = require(`express`);
-const path = requrie(`path`);
+const path = require(`path`);
 
 const articleRoutes = require(`./routes/article-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
 
-const { EXPRESS_DEFAULT_PORT } = require(`../constants`);
+const {HttpCode, EXPRESS_DEFAULT_PORT, PUBLIC_DIR } = require(`../constants`);
 
 const app = express();
 
@@ -18,5 +18,13 @@ app.set(`view engine`, `pug`);
 app.use(`/articles`, articleRoutes);
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).render(`404`);
+});
+
+app.use((req, res) => {
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`500`);
+});
 
 app.listen(EXPRESS_DEFAULT_PORT);
